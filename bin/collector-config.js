eee54864c5cd73cc17f81ddaa3d55386cfd2f54f
Mike Bostock <mbostock@gmail.com>
2012-04-16 18:23:57 -0700

    Clarify default configuration.

--- bin/collector-config.js
+++ bin/collector-config.js
@@ -3,5 +3,7 @@ module.exports = {
   "mongo-host": "127.0.0.1",
   "mongo-port": 27017,
   "mongo-database": "cube_development",
+  "mongo-username": null,
+  "mongo-password": null,
   "http-port": 1080
 };
