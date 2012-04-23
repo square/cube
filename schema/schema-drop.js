0e39a1d03c8a7f3e305505ab308cab1cbc1d8e6e
Mike Bostock <mbostock@gmail.com>
2012-04-23 14:37:41 -0700

    Remove defunct schema.

--- schema/schema-drop.js
+++
@@ -1,6 +0,0 @@
-db.boards.drop();
-
-["random", "stock", "collectd_df", "collectd_load", "collectd_interface", "collectd_memory"].forEach(function(type) {
-  db[type + "_events"].drop();
-  db[type + "_metrics"].drop();
-});
