f6cfbc859e971f13dba7aa35e5a11de5acc78d5b
Huston Hoburg <huston.hoburg@gmail.com>
2012-09-19 14:26:27 -0500

    Update streamer, remove tracing from events

--- examples/random-emitter/random-streamer.js
+++ examples/random-emitter/random-streamer.js
@@ -7,7 +7,7 @@ var cube       = require("../../"), // replace with require("cube")
     models     = require("../../lib/cube/models"), Event = models.Event;
 
 var options = {
-  "collector": "ws://127.0.0.1:1080",
+  "collector": "ws://127.0.0.1:6000",
 
   // The offset and duration to backfill, in milliseconds.
   // For example, if the offset is minus four hours, then the first event that
@@ -15,32 +15,36 @@ var options = {
   // recent events based on the step interval, all the way up to the duration.
 
   "event_frequency": 5000, // per second
+  event_batch: 500,
   event_type: "doh"
 };
 
 var emitter = cube.emitter(options["collector"]),
-    step    = 1 / options.event_frequency;
+    step    = 1000 / options.event_frequency,
+    batch   = options.event_batch || (step < 1 ? (1 / step) : 1);
 
 
 function setup_cromulator(){
-  cromulator.start = new Date(Math.floor(new Date() / (1000 * 60 * 60 * 24)) * 1000 * 60 * 60 * 24);
-  cromulator.stop  = new Date(+cromulator.start + (1000 * 60 * 60 * 24));
-  cromulator.step  = 1 / options.event_frequency;
-}
+  var day = 1000 * 60 * 60 * 24;
+  cromulator.start = new Date(Math.floor(new Date() / day) * day);
+  cromulator.stop  = new Date(+cromulator.start + day);
+  cromulator.step  = step;
+ }
 
 metalog.info('emitter', cromulator.report('starting'));
 
 function send(){
-  if(+cromulator.stop <= +(new Date())) setup_cromulator();
+  if (!cromulator.stop || +cromulator.stop <= +(new Date())) setup_cromulator();
+  var i = -1;
 
-  var time  = new Date(),
-      event = new Event(options.event_type, time, cromulator.data_at(time));
-  if (+time % 1000 == 0) metalog.info('emitter', {em: emitter.report(), cr: cromulator.report('progress', time)});
-  event.force = true;
-  emitter.send(event.to_request());
+  while (++i < batch) {
+    var time  = new Date(),
+        event = new Event(options.event_type, time, cromulator.data_at(time));
+    if (cromulator.count % options.event_frequency == 0) metalog.info('emitter', {em: emitter.report(), cr: cromulator.report('progress', time)});
+    event.force = true;
+    emitter.send(event.to_request());
+  }
 }
 
-setInterval(send, step);
-
-//metalog.info('emitter', cromulator.report('stopping', new Date()));
-//emitter.close();
+var interval = 1000 / (options.event_frequency / batch);
+setInterval(send, interval);
